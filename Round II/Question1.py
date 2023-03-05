f3e3f2aa5329c609248b5524d892673fe7186165
Jane Mpho Kuali <janemphokuali@gmail.com>
2023-03-05 22:41:45 +0200

    Add files via upload

---
+++ Round II/Question1.py
@@ -0,0 +1,23 @@
+import pandas as pd #dependency
+import numpy as np #dependency
+
+df_teacher = pd.DataFrame({
+    "name": ["Pep Guardiola", "Jurgen Klopp", "Mikel Arteta", "Zinadine Zidane"],
+    "married": [True, True, False, True],
+    "school": ["Manchester High School", "Liverpool High School", "Arsenal High", np.nan]
+})
+
+df_student = pd.DataFrame({
+    "teacher": ["Mikel Arteta", "Mikel Arteta", "Pep Guardiola", "Jurgen Klopp",
+                "Jurgen Klopp", "Jurgen Klopp", "Pep Guardiola","Pep Guardiola","Mikel Arteta"],
+    "name": ["Bukayo Saka", "Gabriel Martinelli", "Jack Grealish", "Roberto Firmino",
+            "Andrew Robertson", "Darwin Nunez", "Ederson Moraes", "Manuel Akanji", "Thomas Partey"],
+    "age": [21, 21, 27, 31, 28, 23, 29, 27, 29],
+    "height": ['2.1m','2.1m', '2.1m', '2.1m', '2.1m', '2.1m', '2.1m', '2.1m', '2.1m']
+})
+#print(df_student.to_json(orient='split'))
+
+all = (pd.merge(df_teacher,df_student.rename(columns={'name':'teacher'}),on='teacher',how='right'))
+print(all.to_json())
+# print("\n")
+# print(df_student.to_json(orient = "records"))
